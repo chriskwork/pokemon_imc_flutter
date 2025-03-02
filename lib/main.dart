6579378817c0e0077b17131447636faadc8f5287
chris k <yovirinc@gmail.com>
2025-03-02 14:41:12 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_imc_flutter/screens/home_screen.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Pokemon IMC')),
+        body: const Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
